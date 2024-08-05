614e806a9f73e38a12b228a7e10b16f4f8da667e
silber175 <y.silber175@gmail.com>
2024-08-06 01:05:14 +0300

    Task4 commit 2

--- src/main/java/Kruchkov/Task4/User.java
+++ src/main/java/Kruchkov/Task4/User.java
@@ -20,7 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
     @Entity
-    @Table(name="Users")
+    @Table(name="user")
 
 
     public class User {
